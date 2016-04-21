247f2b5f72a141c430f53599096d82a7aee30e60
adilos-geekos <adil.elmansouri.88@gmail.com>
2016-04-21 03:48:23 -1000

    Initial commit

---
+++ app/src/main/java/com/elmansouriadil/adilosfragment/adilosfragment/Activity_Adilos.java
@@ -0,0 +1,13 @@
+package com.elmansouriadil.adilosfragment.adilosfragment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Adilos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__adilos);
+    }
+}
